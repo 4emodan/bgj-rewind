5b780027ebc93e53c2586102ff58ab6845d4248c
Artyom Lonsky <artyom.lonsky@gmail.com>
2020-08-05 01:42:48 +0300

    Refactor scene setup; Allow multiple films;

--- Assets/Scripts/FilmController.cs
+++ Assets/Scripts/FilmController.cs
@@ -8,10 +8,19 @@ public class FilmController : MonoBehaviour
 
     private Vector2 lastMousePosition = Vector2.zero;
     private bool touchStarted = false;
+    private Rect filmRect;
+
+    private void Start() {
+        var size = transform.localScale.xy();
+        filmRect = new Rect(transform.position.xy() - size / 2f, size);
+        Debug.Log($"{name}: {filmRect}");
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        var mousePosition = getMousePosition();
+        
+        if (Input.GetMouseButtonDown(0) && isPointInside(mousePosition))
         {
             touchStarted = true;
             lastMousePosition = getMousePosition();
@@ -24,19 +33,17 @@ public class FilmController : MonoBehaviour
 
         if (touchStarted)
         {
-            var mousePosition = getMousePosition();
-            // Debug.Log(mousePosition);
-
             var delta = mousePosition - lastMousePosition;
             delta.y = 0;
-
-            // var deltaWorld = Camera.main.ScreenToWorldPoint(delta);
-            Debug.Log(delta);
 
             objects.transform.setX(objects.transform.position.x + delta.x);
 
             lastMousePosition = mousePosition;
         }
+    }
+
+    private bool isPointInside(Vector2 point) {
+        return filmRect.Contains(point);
     }
 
     private Vector2 getMousePosition() {
