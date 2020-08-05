5fd1e8ce6cc2163aa55869b9996d261356c4a1f5
frogsrop <ugai1yanis1@gmail.com>
2020-08-05 03:08:52 +0300

    platforms

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public readonly Vector3 start = new Vector3(0, 0, 0);
+    public Vector3[] points = new Vector3[0];
+}
---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public Path path = null;
+    public float speed = 1;
+
+    private int currentPoint = 0;
+    private int direction = 1;
+    private Vector3[] fullPath = null;
+    private Vector3 currentPosition;
+    private Vector3 delta;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var collider = collision.collider;
+        if (collider.tag == "Player") {
+            collider.transform.SetParent(transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        var collider = collision.collider;
+        if (collider.tag == "Player")
+        { 
+            collider.transform.SetParent(null);
+        }
+    }
+
+    void Start()
+    {
+        currentPosition = Vector3.zero;
+    }
+    void Update()
+    {
+        if (path == null)
+        {
+            fullPath = new Vector3[1];
+            fullPath[0] = Vector3.zero;
+        }
+        else
+        {
+            if (currentPoint == 0 || currentPoint == fullPath.Length - 1)
+            {
+                fullPath = new Vector3[path.points.Length + 1];
+                fullPath[0] = path.start;
+                for (int i = 0; i < path.points.Length; i++)
+                {
+                    fullPath[i + 1] = path.points[i];
+                }
+            }
+        }
+        if (path == null)
+        {
+            return;
+        }
+        int nextPoint = currentPoint + direction;
+        if (nextPoint == -1 || nextPoint == fullPath.Length)
+        {
+            direction *= -1;
+        }
+        nextPoint = currentPoint + direction;
+        if (Vector3.Magnitude(currentPosition - fullPath[nextPoint]) < 0.1f)
+        {
+            currentPoint = nextPoint;
+        }
+        Vector3 directionVec = fullPath[nextPoint] - currentPosition;
+        delta = directionVec.normalized * Time.deltaTime * speed;
+    }
+
+    void FixedUpdate()
+    {
+        currentPosition += delta;
+        this.transform.Translate(delta);
+    }
+}
